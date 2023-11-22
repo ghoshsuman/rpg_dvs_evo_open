e85f143b7128aa1cec054a405d21b261dbdbacbb
ValentinaCavinato <cavinatovalentina@gmail.com>
2023-11-22 11:25:54 +0100

    Fix keypresses and reset rovio

--- rqt_evo/src/rqt_evo/keypresses.py
+++ rqt_evo/src/rqt_evo/keypresses.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python
+import keyboard
 import os
 import rospy
-import rospkg
 import numpy as np
-from python_qt_binding import loadUi
-from python_qt_binding.QtWidgets import QWidget
-from python_qt_binding.QtCore import QTimer, Slot
-#from evo_msgs.msg import Info
 from std_msgs.msg import String, Bool
 import subprocess
-from PyQt5.QtCore import Qt
+import time
 
 
-class EvoWidget(QWidget):
+class KeyPressCommand():
     """
     EVO rqt widget actions
     """
@@ -20,55 +16,20 @@ class EvoWidget(QWidget):
     _publisher = None
     _subscriber = None
     _publisher_copilot = None
-    _num_received_msgs = 0
-    _evo_namespace = None
 
     def __init__(self, evo_namespace='evo'):
 
-        # Init QWidget
-        super(EvoWidget, self).__init__()
-        self.setObjectName('EvoWidget')
-
-        # load UI
-        ui_file = os.path.join(rospkg.RosPack().get_path(
-            'rqt_evo'), 'resource', 'widget.ui')
-        loadUi(ui_file, self)
-
-        # init and start update timer for data, the timer calls the function update_info all 40ms
-        #self._update_info_timer = QTimer(self)
-        # self._update_info_timer.timeout.connect(self.update_info)
-        # self._update_info_timer.start(40)
-
-        # set the functions that are called when a button is pressed
-        self.button_bootstrap.pressed.connect(self.on_bootstrap_button_pressed)
-        self.button_start.pressed.connect(self.on_start_button_pressed)
-        self.button_update.pressed.connect(self.on_update_button_pressed)
-        self.button_switch.pressed.connect(self.on_switch_button_pressed)
-        self.checkbox_map_expansion.stateChanged.connect(
-            self.on_map_expansion_changed)
-        self.checkbox_copilot.stateChanged.connect(
-            self.on_copilot_state_changed)
-
-        # set callback for changed topic
 
         if not evo_namespace:
             evo_namespace = 'evo'
 
         self._evo_namespace = evo_namespace
-        self.topic_line_edit.textChanged.connect(self._on_topic_changed)
         self.register(evo_namespace)
-        self.topic_line_edit.setText(evo_namespace)
+        self.useEVO = False
+        self.checkbox_map_expansion = True
+        self.append_to_pc = True
+        rospy.init_node('keypresser', anonymous=True)
 
-        # TODO: set a timer when the last message was received and give a warning if it is too long ago!
-
-    @Slot(str)
-    def _on_topic_changed(self, topic):
-        """
-        Switches publishers namespaces
-        """
-        self._evo_namespace = str(topic)
-        self.unregister()
-        self.register(self._evo_namespace)
 
     def register(self, evo_namespace):
         """
@@ -98,20 +59,6 @@ class EvoWidget(QWidget):
             self._subscriber.unregister()
             self._subscriber = None
 
-    # def info_cb(self, msg):
-    #  self._last_info_msg = msg
-    #  self._num_received_msgs += 1
-
-    # def update_info(self):
-    #  info_text = 'Not Connected'
-    #  if self._num_received_msgs > 0:
-    #    pass
-
-        # set info text
-        # self.evo_info_label.setText(info_text)
-
-        # set progress bar
-        # self.num_tracked_bar.setValue(self._last_info_msg.num_matches)
 
     def on_bootstrap_button_pressed(self):
         """
@@ -145,20 +92,27 @@ class EvoWidget(QWidget):
         """
         Switches on and off the map expansion algorithm based on checkbox_map_expansion 
         """
-        if self.checkbox_map_expansion.isChecked():
+        if self.checkbox_map_expansion:
             print('ENABLE MAP EXPANSION')
             self.send_command('enable_map_expansion')
         else:
             print('DISABLE EXPANSION')
             self.send_command('disable_map_expansion')
+        self.checkbox_map_expansion = not self.checkbox_map_expansion
+
+    def on_pointcloud_update_changed(self):
+        self.send_command("global_pc_switch")
+        self.append_to_pc = not self.append_to_pc
+        print("APPEND TO POINTCLOUD: ", self.append_to_pc)
 
     def on_copilot_state_changed(self):
         """
         Switch from bootstrapping tracker to evo tracker
         """
-        useEVO = self.checkbox_copilot.isChecked()
-        print('SWITCH COPILOT TO ' + ('EVO' if useEVO else 'INITIAL PILOT'))
-        self._publisher_copilot.publish(Bool(useEVO))
+
+        print('SWITCH COPILOT TO ' + ('EVO' if self.useEVO else 'INITIAL PILOT'))
+        self._publisher_copilot.publish(Bool(self.useEVO))
+        self.useEVO = not self.useEVO
 
     def send_command(self, cmd):
         """
@@ -167,3 +121,44 @@ class EvoWidget(QWidget):
         if self._publisher is None:
             return
         self._publisher.publish(String(cmd))
+
+
+if __name__=="__main__":
+    keypresser = KeyPressCommand()
+    is_enabled = True
+
+
+    while True:
+        if keyboard.is_pressed("x"):
+            is_enabled = not is_enabled
+            print("listening to the keyboard is ", is_enabled)
+            time.sleep(1)
+
+        if not is_enabled:
+            continue
+        if keyboard.is_pressed("t"):
+            keypresser.on_start_button_pressed()
+            time.sleep(0.2)
+            keypresser.on_switch_button_pressed()
+        if keyboard.is_pressed("m"):
+            keypresser.on_map_expansion_changed()
+            time.sleep(0.5)
+        if keyboard.is_pressed("e"):
+            keypresser.on_copilot_state_changed()
+            time.sleep(0.5)
+        if keyboard.is_pressed("u"):
+            keypresser.on_update_button_pressed()
+            time.sleep(0.5)
+        if keyboard.is_pressed("k"):
+            break
+        if keyboard.is_pressed("r"):
+            keypresser.send_command("reset_rovio")
+            print("resetting ROVIO")
+            time.sleep(0.5)
+        if keyboard.is_pressed("p"):
+            keypresser.on_pointcloud_update_changed()
+            time.sleep(0.5)
+
+        
+
+

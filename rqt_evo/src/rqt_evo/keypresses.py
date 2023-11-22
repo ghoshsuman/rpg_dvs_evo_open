95bcb85b12651db3d3314d4a1fc5617fccd444e9
ValentinaCavinato <cavinatovalentina@gmail.com>
2023-11-22 13:15:07 +0100

    publish available commands as an image

--- rqt_evo/src/rqt_evo/keypresses.py
+++ rqt_evo/src/rqt_evo/keypresses.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 import keyboard
-import os
 import rospy
 import numpy as np
 from std_msgs.msg import String, Bool
-import subprocess
 import time
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+from typing import Optional, Tuple
 
 
 class KeyPressCommand():
@@ -29,6 +31,7 @@ class KeyPressCommand():
         self.checkbox_map_expansion = True
         self.append_to_pc = True
         rospy.init_node('keypresser', anonymous=True)
+        self.text_to_write = "\nt: reset evs tracking\n" + "m: switch map expansion\n" + "p: switch append to pointcloud\n" + "u: update map\n" + "e: switch copilot\n" +"r: reset rovio\n" + "k: quit this program\n" 
 
 
     def register(self, evo_namespace):
@@ -92,13 +95,13 @@ class KeyPressCommand():
         """
         Switches on and off the map expansion algorithm based on checkbox_map_expansion 
         """
+        self.checkbox_map_expansion = not self.checkbox_map_expansion
         if self.checkbox_map_expansion:
             print('ENABLE MAP EXPANSION')
             self.send_command('enable_map_expansion')
         else:
             print('DISABLE EXPANSION')
             self.send_command('disable_map_expansion')
-        self.checkbox_map_expansion = not self.checkbox_map_expansion
 
     def on_pointcloud_update_changed(self):
         self.send_command("global_pc_switch")
@@ -110,9 +113,9 @@ class KeyPressCommand():
         Switch from bootstrapping tracker to evo tracker
         """
 
+        self.useEVO = not self.useEVO
         print('SWITCH COPILOT TO ' + ('EVO' if self.useEVO else 'INITIAL PILOT'))
         self._publisher_copilot.publish(Bool(self.useEVO))
-        self.useEVO = not self.useEVO
 
     def send_command(self, cmd):
         """
@@ -122,42 +125,154 @@ class KeyPressCommand():
             return
         self._publisher.publish(String(cmd))
 
+    def write_image(self, image, additional_text = ""):
+        if(additional_text!=""):
+            print(additional_text)
+        text_to_write = self.text_to_write + additional_text
+        return add_text_to_image(image, text_to_write)
+
+
+
+def add_text_to_image(
+    image_rgb: np.ndarray,
+    label: str,
+    top_left_xy: Tuple = (0, 0),
+    font_scale: float = 1,
+    font_thickness: float = 3,
+    font_face=cv2.FONT_HERSHEY_SIMPLEX,
+    font_color_rgb: Tuple = (255, 255, 0),
+    bg_color_rgb: Optional[Tuple] = None,
+    outline_color_rgb: Optional[Tuple] = None,
+    line_spacing: float = 1,
+):
+    """
+    Adds text (including multi line text) to images.
+    You can also control background color, outline color, and line spacing.
+
+    outline color and line spacing adopted from: https://gist.github.com/EricCousineau-TRI/596f04c83da9b82d0389d3ea1d782592
+    """
+    OUTLINE_FONT_THICKNESS = 3 * font_thickness
+
+    im_h, im_w = image_rgb.shape[:2]
+
+    for line in label.splitlines():
+        x, y = top_left_xy
+
+        # ====== get text size
+        if outline_color_rgb is None:
+            get_text_size_font_thickness = font_thickness
+        else:
+            get_text_size_font_thickness = OUTLINE_FONT_THICKNESS
+
+        (line_width, line_height_no_baseline), baseline = cv2.getTextSize(
+            line,
+            font_face,
+            font_scale,
+            get_text_size_font_thickness,
+        )
+        line_height = line_height_no_baseline + baseline
+
+        if bg_color_rgb is not None and line:
+            # === get actual mask sizes with regard to image crop
+            if im_h - (y + line_height) <= 0:
+                sz_h = max(im_h - y, 0)
+            else:
+                sz_h = line_height
+
+            if im_w - (x + line_width) <= 0:
+                sz_w = max(im_w - x, 0)
+            else:
+                sz_w = line_width
+
+            # ==== add mask to image
+            if sz_h > 0 and sz_w > 0:
+                bg_mask = np.zeros((sz_h, sz_w, 3), np.uint8)
+                bg_mask[:, :] = np.array(bg_color_rgb)
+                image_rgb[
+                    y : y + sz_h,
+                    x : x + sz_w,
+                ] = bg_mask
+
+        # === add outline text to image
+        if outline_color_rgb is not None:
+            image_rgb = cv2.putText(
+                image_rgb,
+                line,
+                (x, y + line_height_no_baseline),  # putText start bottom-left
+                font_face,
+                font_scale,
+                outline_color_rgb,
+                OUTLINE_FONT_THICKNESS,
+                cv2.LINE_AA,
+            )
+        # === add text to image
+        image_rgb = cv2.putText(
+            image_rgb,
+            line,
+            (x, y + line_height_no_baseline),  # putText start bottom-left
+            font_face,
+            font_scale,
+            font_color_rgb,
+            font_thickness,
+            cv2.LINE_AA,
+        )
+        top_left_xy = (x, y + int(line_height * line_spacing))
+
+    return image_rgb
 
 if __name__=="__main__":
     keypresser = KeyPressCommand()
     is_enabled = True
-
-
+    cv_bridge = CvBridge()
+    image_publisher = rospy.Publisher("/evo/commands", Image, queue_size=10)
     while True:
+        additional_text = ""
         if keyboard.is_pressed("x"):
             is_enabled = not is_enabled
             print("listening to the keyboard is ", is_enabled)
             time.sleep(1)
+        
+        def write_and_publish(text, sleep=0.7):
+            image = np.zeros((512,512,3), np.uint8)
+            image = keypresser.write_image(image, text)
+            image_publisher.publish(cv_bridge.cv2_to_imgmsg(image))
+            time.sleep(sleep)
 
         if not is_enabled:
             continue
         if keyboard.is_pressed("t"):
             keypresser.on_start_button_pressed()
-            time.sleep(0.2)
+            additional_text += "\n\n\nReset tracking...\n"
+            write_and_publish(additional_text)
             keypresser.on_switch_button_pressed()
         if keyboard.is_pressed("m"):
             keypresser.on_map_expansion_changed()
-            time.sleep(0.5)
+            additional_text += "\n\n\nSwitched map expansion: {}\n".format(keypresser.checkbox_map_expansion)
+            write_and_publish(additional_text)
         if keyboard.is_pressed("e"):
             keypresser.on_copilot_state_changed()
-            time.sleep(0.5)
+            additional_text += "\n\n\nCopilot changed to: {}\n".format(keypresser.useEVO)
+            write_and_publish(additional_text)
         if keyboard.is_pressed("u"):
             keypresser.on_update_button_pressed()
-            time.sleep(0.5)
+            additional_text += "\n\n\nUpdate map command sent\n"
+            write_and_publish(additional_text)
         if keyboard.is_pressed("k"):
             break
         if keyboard.is_pressed("r"):
             keypresser.send_command("reset_rovio")
+            additional_text += "\n\n\nResetting rovio..\n"
             print("resetting ROVIO")
-            time.sleep(0.5)
+            write_and_publish(additional_text)
         if keyboard.is_pressed("p"):
             keypresser.on_pointcloud_update_changed()
-            time.sleep(0.5)
+            additional_text += "\n\n\nAppend to pointcloud: {}".format(keypresser.append_to_pc)
+            write_and_publish(additional_text)
+            
+        write_and_publish("", 0.001)
+            
+        
+
 
         
 
